d9c771e9b432479b12e0253bdd3bc9585ef450f7
Pablo Adriano <pabloadrianomc@gmail.com>
2021-11-19 21:55:13 -0300

    style: landing page interface created

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,73 @@
+import {
+  Box,
+  CssBaseline,
+  Container,
+  Button,
+  Avatar,
+  Typography,
+  TextField,
+} from "@mui/material";
+import { Lock } from "@mui/icons-material";
+import useStyles from "./styles";
+
 const Login = () => {
-  return <h1>Login</h1>;
+  const classes = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <Container component="main" maxWidth="xs">
+        <Box className={classes.box} maxWidth="xs">
+          <Avatar
+            sx={{ m: 1, width: 55, height: 55, bgcolor: "secondary.main" }}
+          >
+            <Lock />
+          </Avatar>
+          <Typography component="h1" variant="h4">
+            Login
+          </Typography>
+          <Box component="form">
+            <TextField
+              margin="normal"
+              fullWidth
+              placeholder="Email"
+              label="Email"
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              placeholder="Senha"
+              label="Senha"
+            />
+            <Button
+              className={classes.button}
+              type="submit"
+              fullWidth
+              variant="contained"
+              size="medium"
+            >
+              Entrar
+            </Button>
+          </Box>
+          <Typography
+            variant="body2"
+            align="center"
+            color="textPrimary"
+            paragraph
+          >
+            Criar uma página para mostrar suas habilidades, metas e progresso
+          </Typography>
+          <Button
+            className={classes.button}
+            fullWidth
+            variant="outlined"
+            size="medium"
+          >
+            Cadastrar
+          </Button>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default Login;
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  box: {
+    marginTop: 80,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  button: {
+    marginTop: 12,
+    marginBottom: 14,
+  },
+});
+
+export default useStyles;
